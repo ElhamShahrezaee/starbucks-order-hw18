511dbc9c2c415f4dbade1762d436209fc076bdb1
ElhamShahrezaee <eli.shahrezaee1998@gmail.com>
2025-12-24 14:40:57 +0330

    feat: add header with starbucks title

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="text-center py-6">
+      <h1 className="text-3xl font-bold text-green-700">
+        Starbucks Online Coffee Order
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
