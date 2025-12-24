edadc3831e96b3b260c8e410b3a8dfea613aa2af
ElhamShahrezaee <eli.shahrezaee1998@gmail.com>
2025-12-24 14:56:29 +0330

    feat: implement cart logic and total price calculation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Header from "./components/Header";
+import ProductCard from "./components/ProductCard";
+import { products } from "./data/products";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cart, setCart] = useState<{ [key: number]: number }>({});
+
+  const increase = (id: number) => {
+    setCart({ ...cart, [id]: (cart[id] || 0) + 1 });
+  };
+
+  const decrease = (id: number) => {
+    if (!cart[id]) return;
+    setCart({ ...cart, [id]: cart[id] - 1 });
+  };
+
+  const totalPrice = products.reduce((total, product) => {
+    return total + (cart[product.id] || 0) * product.price;
+  }, 0);
+
+  const hasItem = totalPrice > 0;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen flex flex-col justify-between">
+      <Header />
+
+      {/* Products */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4 px-6">
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            quantity={cart[product.id] || 0}
+            onIncrease={() => increase(product.id)}
+            onDecrease={() => decrease(product.id)}
+          />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+
+      {/* Footer */}
+      <div className="bg-gray-100 mt-8 p-6 text-center">
+        <p className="text-xl font-bold">Total: ${totalPrice.toFixed(2)}</p>
+
+        <button
+          disabled={!hasItem}
+          className={`mt-4 px-8 py-2 rounded text-white ${
+            hasItem ? "bg-orange-400" : "bg-gray-400 cursor-not-allowed"
+          }`}
+        >
+          Submit Order
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
